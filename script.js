2f8e21912cad2af463c28c4baf809dbefa7c8492
Dragon93PL <blabla1942@o2.pl>
2020-08-31 09:58:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+var min = [5,6,3,4,7,11,56,43];
+for (var i = 0; i < tab.length; i++) {
+    if (tab[i] < min) {
+        min = tab[i];
+    }
+}
+console.log(min); // -5
+
+
+var max = [5,6,3,4,7,11,56,43];
+for (var i = 0; i < tab.length; i++) {
+    if (tab[i] > max) {
+        max = tab[i];
+    }
+}
+console.log(max); // 11
+
+
+funkcja{
+    for (var i = 1; i < tab.length; i++) {
+    if (tab[i] > max) {
+        max = tab[i];
+    }
+    
+}
+    ------
+ 
+        var liczba;
+      liczba=Math.floor(Math.random()*100+);
+                     
+                        const=prompt;
+                 prompt(odgadnij liczbę,value);
+    
+    if {(liczba===value)
+                  alert('true');
+       }
+ else{
+        alert('false');
+    }
+
+    
+    
+    
+    var liczba;
+    var liczba2
+    var liczba3;
+    
+function licz()
+{
+	a=ff.a.value;
+	b=ff.b.value;
+	c=ff.c.value;
+	
+	if (((a*a)+(b*b)==(c*c))||((c*c)+(b*b)==(a*a))||((a*a)+(c*c)==(b*b)))
+	{
+		document.write("Prostokątny!");
+	}
+	else
+	{
+		document.write("Nieprostokątny!");
+	}
+        
+        
+    var word = 'bdhsdhsdujdd dsjfsdiogj';
+    var splitteWorld = world.split('');
+    var numbers = [0,1,2,3,4,5,6,7,8,9];
+    var result = [];
+    
+    for(let i = 0; i < word.length; i++){
+        if(!isNan(parseInt(word[i]))
+        
+        
+    }
